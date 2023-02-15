8fedb700d17ad8f8169bc81da5e4ce1d65e6e661
Zhanylai <kasymkulovajanylai@gmail.com>
2023-02-15 20:08:28 +0600

    first commit

--- apps/discounts/apps.py
+++ apps/discounts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class OtzyvConfig(AppConfig):
+class DiscountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'otzyv'
+    name = 'apps.discounts'
--- apps/otzyv/apps.py
+++ apps/otzyv/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DiscountsConfig(AppConfig):
+class OtzyvConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'discounts'
+    name = 'apps.otzyv'
