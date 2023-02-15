ba9aa70fa2b844f973418848b2e93eafa4911ff7
Zhanylai <kasymkulovajanylai@gmail.com>
2023-02-15 20:10:41 +0600

    first commit

--- main/settings.py
+++ main/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.discounts',
+    'apps.otzyv',
+    'apps.team',
 ]
 
 MIDDLEWARE = [
