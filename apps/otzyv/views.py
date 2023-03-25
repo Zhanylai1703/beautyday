636c64b22f44bd7e21b4c2f8638bbf70b1411ef6
Alexandra-vic <alexandra20050416@gmail.com>
2023-03-25 16:28:15 +0600

    add reviews

--- apps/otzyv/views.py
+++ apps/otzyv/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from apps.otzyv.models import Review
+from apps.otzyv.serializers import ReviewSerializer
+
+
+class ReviewList(generics.ListAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+class ReviewCreate(generics.CreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer    
--- main/urls.py
+++ main/urls.py
@@ -16,6 +16,39 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from rest_framework import permissions
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+from apps.otzyv.views import (
+    ReviewList,
+    ReviewCreate,
+    ReviewDetail,
+)
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="BeautyDay API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('reviews/', ReviewList.as_view(), name='review-list'),
+    path('review/', ReviewCreate.as_view(), name='review-create'),
+    path('review/<int:pk>/', ReviewDetail.as_view(), name='review-detail'),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc'),
 ]
